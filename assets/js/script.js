28b6e81bfceba156d2d4184552af0db99b622cb4
Lejeesh Krishna <lejeeshkrishna573@gmail.com>
2022-09-03 16:42:25 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,105 @@
+var nameError = document.getElementById('name-error');
+// var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validateName() {
+    var name1 = document.getElementById("contact-name").value;
+
+    if (name1.length == 0) {
+        nameError.innerHTML = "Name is required";
+        return false;
+    }
+
+    if (!name1.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = "write full name";
+        return false;
+    }
+    nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+}
+
+// function validatePhone() {
+//     var phone = document.getElementById("contact-phone").value;
+
+//     if (phone.length == 0) {
+//         phoneError.innerHTML = "Phone no is required";
+//         return false;
+//     }
+//     if (phone.length !== 10) {
+//         phoneError.innerHTML = "Phone no should be 10 digits";
+//         return false
+//     }
+//     if (!phone.match(/^[0-9]{10}$/)) {
+//         phoneError.innerHTML = "only digits can enter";
+//         return false
+//     }
+//     phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+//     return true;
+
+// }
+
+function validateEmail() {
+    var email = document.getElementById("contact-email").value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = "Email is required";
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = "email is invalid";
+        return false
+    }
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+}
+function validateMessage() {
+    var msg = document.getElementById("contact-message").value;
+    var required = 30;
+    var left = required - msg.length;
+
+    if (msg.length == 0) {
+        messageError.innerHTML = "Message  is required";
+         return false;
+     }
+
+
+    if (left > 0) {
+        messageError.innerHTML = left + 'more characters required';
+        return false;
+
+    }
+    messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+
+
+      
+
+}
+function validateForm() {
+    
+    if (!validateName() && !validateEmail() && !validateMessage()) {
+        nameError.innerHTML = "Name is required";
+        emailError.innerHTML = "Email is required";
+        messageError.innerHTML = "Message  is required";
+    }
+
+
+    if(!validateName() || !validateEmail() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please clear the errors';
+        setTimeout(function () { submitError.style.display = 'none'; }, 3000);
+        return false;
+    }
+
+    
+    // submitError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    // return true;
+
+}
+
+
